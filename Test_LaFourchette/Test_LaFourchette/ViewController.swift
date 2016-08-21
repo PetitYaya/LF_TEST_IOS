b809221cd79b332888ace362187b9ec92b12140c
AppleAddict <yacine.yazman@gmail.com>
2016-08-21 09:02:32 +0200

    [YYA] : Setup Xcode Project (iOS 8+ Deployment Target) + Init CocoaPods

---
+++ Test_LaFourchette/Test_LaFourchette/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test_LaFourchette
+//
+//  Created by Yacine on 21/08/2016.
+//  Copyright © 2016 Yacine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
